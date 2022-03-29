1f08e354fd769f6284a259d7317cdfbce945b5ad
xencodes <xencodes@icloud.com>
2022-03-29 21:53:03 +0530

    feat: init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,13 @@
+// simple express
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`server is listening on http://localhost:${port}`);
+});
