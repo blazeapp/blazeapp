d4d3238375c366d09729b38079e0097782fbc85c
xencodes <xencodes@icloud.com>
2022-04-02 10:31:23 +0530

    chore: format

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,9 +5,9 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-	console.log(`server is listening on http://localhost:${port}`);
+  console.log(`server is listening on http://localhost:${port}`);
 });
